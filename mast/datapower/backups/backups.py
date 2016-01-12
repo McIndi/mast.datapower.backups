932723b1737e6075233d72e9cfbd569d70c02b6d
steveyeof <stephenkearney@mcindi.com>
2016-01-11 20:05:06 -0500

    Trying this

--- mast/datapower/backups/backups.py
+++ mast/datapower/backups/backups.py
@@ -310,7 +310,7 @@ or one set for each appliance. Credentials should be in the form
 if multiple are provided. When referencing multiple appliances with
 multiple credentials there must be a one to one correspondence of 
 credentials to appliances:
-`[appliance1 [appliance2...]] [credential1 [credential2...]]` 
+<br />`[appliance1 [appliance2...]] [credential1 [credential2...]]` <br />
 If you would prefer to not use plain-text passwords, 
 you can use the output of `$ mast-system xor <username:password>`.
 * `-t, --timeout`: The timeout in seconds to wait for a response from
