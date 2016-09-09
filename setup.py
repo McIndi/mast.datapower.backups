3307706840b4cfcd74dd5efb9a1ef91ebe44d699
ilovetux <me@ilovetux.com>
2016-09-09 13:51:29 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="mast.datapower.backups",
-    version="2.1.0",
+    version="2.2.0",
     author="Clifford Bressette",
     author_email="cliffordbressette@mcindi.com",
     description=(
